75580650a77ff482401a757c28efb65444dbcbf6
denoc1 <176614645+denoc1@users.noreply.github.com>
2024-09-09 01:01:55 +0000

    template lab2

---
+++ lab2.java
@@ -0,0 +1,9 @@
+public class lab2{
+    public static void main (String[] args){
+            //write your code here - inside the driver method
+
+
+            
+
+    }  //end of main (Driver method)
+}//end of class
